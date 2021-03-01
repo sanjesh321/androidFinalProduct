d7f3eb67c30f73e379a96d3b7b051be45daaf357
sanjesh321 <danashu88@gmail.com>
2021-03-01 14:08:01 +0545

    new

---
+++ app/src/main/java/com/sanjesh/motomart/LogIN.kt
@@ -0,0 +1,11 @@
+package com.sanjesh.motomart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIN : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_i_n)
+    }
+}
