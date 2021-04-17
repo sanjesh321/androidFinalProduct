112017991a9cf3c99e608d31bd0481dae5e566a0
sanjesh321 <danashu88@gmail.com>
2021-04-18 01:04:36 +0545

    added home adapter

--- app/src/main/java/com/sanjesh/motomart/Adapter/Home_Adapter.kt
+++ app/src/main/java/com/sanjesh/motomart/Adapter/Home_Adapter.kt
@@ -1,11 +1,40 @@
 package com.sanjesh.motomart.Adapter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class Home_Adapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home__adapter)
+class Home_Adapter(private val lstCategory: ArrayList<Categories>,
+                  val context: Context
+): RecyclerView.Adapter<Home_Adapter.CategoriesViewHolder>() {
+    class CategoriesViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val ivCat: ImageView
+        val catName: TextView
+        init {
+            ivCat = view.findViewById(R.id.ivCat)
+            catName=view.findViewById(R.id.catName)
+        }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_category,parent,false)
+        return CategoriesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
+        val Categories = lstCategory[position]
+        holder.catName.text = Categories.catName
+        Glide.with(context).load(Categories.catImg).into(holder.ivCat)
+    }
+
+    override fun getItemCount(): Int {
+        return lstCategory.size
+    }
+
 }
