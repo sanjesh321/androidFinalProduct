f63c19a993176e4dbb5b33411aa9297125b34c88
sanjesh321 <danashu88@gmail.com>
2021-04-18 00:58:07 +0545

    added static cart

---
+++ app/src/main/java/com/sanjesh/motomart/API/Static_cart.kt
@@ -0,0 +1,9 @@
+package com.sanjesh.motomart.API
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+object Static_cart {
+    var lstCart:MutableList<Cart> = mutableListOf()
+    var myCart:MutableList<Cart> = mutableListOf()
+}
