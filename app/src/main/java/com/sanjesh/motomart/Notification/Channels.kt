1ae6b12053c1162415978189544700d0428ae03f
sanjesh321 <danashu88@gmail.com>
2021-04-18 01:16:23 +0545

    added notification channel

--- app/src/main/java/com/sanjesh/motomart/Notification/Channels.kt
+++ app/src/main/java/com/sanjesh/motomart/Notification/Channels.kt
@@ -1,11 +1,22 @@
 package com.sanjesh.motomart.Notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Channels : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_channels)
+class Channels(val context: Context) {
+    val channel1:String = "Channel1"
+    val channel2:String="Channel2"
+    fun createNotificationChannels()
+    {
+        val channel_1 = NotificationChannel(channel1,"highChannel", NotificationManager.IMPORTANCE_HIGH)
+        channel_1.description = "Notification from Channel 1"
+        val channel_2 = NotificationChannel(channel2,"lowChannel", NotificationManager.IMPORTANCE_LOW)
+        channel_2.description = "Notification from Channel 2"
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel_1)
+        notificationManager.createNotificationChannel(channel_2)
     }
 }
