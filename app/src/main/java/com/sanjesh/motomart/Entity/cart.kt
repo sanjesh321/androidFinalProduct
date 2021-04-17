167a2a7292cf59b02295b29639f562046b45c8de
sanjesh321 <danashu88@gmail.com>
2021-04-18 00:49:14 +0545

    added entity cart

---
+++ app/src/main/java/com/sanjesh/motomart/Entity/cart.kt
@@ -0,0 +1,16 @@
+package com.sanjesh.motomart.Entity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class cart(
+    @PrimaryKey
+    val _id:String = "",
+    val p_id:Product?=null,
+    val u_id:String?=null,
+    var pprice:Int=0,
+    var pquantity:Int=0
+)
