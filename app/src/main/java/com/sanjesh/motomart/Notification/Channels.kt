aa50e829eeeb49362423cff9f73a5b4f5106ac74
sanjesh321 <danashu88@gmail.com>
2021-04-18 10:41:49 +0545

    edited errors of dependencies

--- app/src/main/java/com/sanjesh/motomart/Notification/Channels.kt
+++ app/src/main/java/com/sanjesh/motomart/Notification/Channels.kt
@@ -3,17 +3,20 @@ package com.sanjesh.motomart.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 
 class Channels(val context: Context) {
     val channel1:String = "Channel1"
     val channel2:String="Channel2"
+    @RequiresApi(Build.VERSION_CODES.O)
     fun createNotificationChannels()
     {
-        val channel_1 = Channels(channel1,"highChannel", NotificationManager.IMPORTANCE_HIGH)
+        val channel_1 = NotificationChannel(channel1,"highChannel", NotificationManager.IMPORTANCE_HIGH)
         channel_1.description = "Notification from Channel 1"
-        val channel_2 = Channels(channel2,"lowChannel", NotificationManager.IMPORTANCE_LOW)
+        val channel_2 = NotificationChannel(channel2,"lowChannel", NotificationManager.IMPORTANCE_LOW)
         channel_2.description = "Notification from Channel 2"
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel_1)
